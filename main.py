28fc3ef702f0e252f8adeee9cf636215cfd96d88
ShubhaM <81143367+shubham-1319@users.noreply.github.com>
2023-12-31 23:41:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def register_user():
+    name = input("Enter your name: ")
+    email = input("Enter your Gmail address: ")
+    return name, email
+
+def main():
+    print("Welcome! Please register:")
+    user_details = register_user()
+    print("\nRegistration successful!")
+    print("User Details:")
+    print("Name:", user_details[0])
+    print("Gmail:", user_details[1])
+
+if __name__ == "__main__":
+    main()
